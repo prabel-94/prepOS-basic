6771abece8e6164a9afb54a7c365016debcb929a
prabel-94 <prabelsurendran@gmail.com>
2026-03-08 20:06:51 +0530

    Update exam.js

--- js/exam.js
+++ js/exam.js
@@ -604,4 +604,17 @@ function removePDFHeader(){
 
 }
 
+async function imageToBase64(url){
+
+  const res = await fetch(url);
+  const blob = await res.blob();
+
+  return new Promise((resolve)=>{
+    const reader = new FileReader();
+    reader.onloadend = ()=> resolve(reader.result);
+    reader.readAsDataURL(blob);
+  });
+
+}
+
 document.addEventListener("DOMContentLoaded", loadExam);
