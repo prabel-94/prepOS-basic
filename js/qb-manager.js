eb5020535fc946438eb8ba75638178b6e317dca4
prabel-94 <prabelsurendran@gmail.com>
2026-03-29 16:29:09 +0530

    Update qb-manager.js

--- js/qb-manager.js
+++ js/qb-manager.js
@@ -222,7 +222,9 @@ function renderTopics() {
 
     return `
       <div class="question-card ${weak}">
-        <div class="q-title">${t.name}</div>
+        <div class="q-title">
+  ${t.name} (${t.count})
+</div>
         <div class="small mt-5">${t.count} questions</div>
 
         <button class="secondary-btn mt-10 view-topic-btn" data-id="${t.id}">
