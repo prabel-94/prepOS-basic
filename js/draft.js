aeb93d7ef23d2ca191fb17a49e56aedadfa1db61
prabel-94 <prabelsurendran@gmail.com>
2026-04-04 00:22:04 +0530

    Update draft.js

--- js/draft.js
+++ js/draft.js
@@ -391,6 +391,7 @@ async function addAllResultsToDraft() {
     score: null,
     label: null
   }
+ensureMetadata(newQuestion);
     };
 
     currentDraft.schema_json.sections[0].questions.push(newQuestion);
